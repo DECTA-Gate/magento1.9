90bff6ed658aa0181cc15757358d0e9167d89f90
nbrouka <nadzeya.brouka@gmail.com>
2020-09-29 08:26:45 +0300

    added ignore ssl

--- code/local/Decta/Decta/lib/decta_api.php
+++ code/local/Decta/Decta/lib/decta_api.php
@@ -115,6 +115,7 @@ Class DectaAPI
             curl_setopt($ch, CURLOPT_URL, $request);
         }
 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
         curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
